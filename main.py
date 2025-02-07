a635c9c10ebc0ce3e573a4748ebcebf7a6987af1
Flayton Soares <104874690+FlayBoss@users.noreply.github.com>
2025-02-07 10:52:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ rec = sr.Recognizer()
 with sr.Microphone(device_index=3) as microfone:
     rec.adjust_for_ambient_noise(microfone)
     print("Pode começar a falar:")
+    
     # rec.pause_threshold = 1 # coloque isso se quiser que ele demore mais para entender que uma pausa é o fim do audio
+    
     audio = rec.listen(microfone)
 try:
     # reconhece esse audio e traduz ele pra texto
